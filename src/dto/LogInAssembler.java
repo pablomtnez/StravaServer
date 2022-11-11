37cc2589ef2e2560360952b1d9dd5de8a7493440
Pablo Martínez <pablomtneez@opendeusto.es>
2022-11-11 10:26:45 +0100

    Login Assembler y DTO

--- src/dto/LogInAssembler.java
+++ src/dto/LogInAssembler.java
@@ -1,5 +1,27 @@
 package dto;
 
-public class LogInAssembler {
+import clases.Login;
 
+public class LogInAssembler {
+	
+	private static LogInAssembler instance;
+	
+	private LogInAssembler() { }
+	
+	public static LogInAssembler getInstance() {
+		if (instance ==  null) {
+			instance = new LogInAssembler();
+		}
+		return instance;
+		
+	}
+	
+public LogInDTO LogInToDTO (Login login) {
+		
+		LogInDTO dto = new LogInDTO();
+		
+		dto.setEmail(null);
+		return dto;
+	}
+	
 }
--- src/dto/LogInDTO.java
+++ src/dto/LogInDTO.java
@@ -1,5 +1,19 @@
 package dto;
 
-public class LogInDTO {
+import java.io.Serializable;
 
+public class LogInDTO implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private String email;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
 }
