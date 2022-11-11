1a761f1c0f4d5298bc6a2cfd752e7d48f6b525db
mariaMardones <115789955+mariaMardones@users.noreply.github.com>
2022-11-11 10:19:11 +0100

    RetoAssembler

--- src/dto/RetoAssembler.java
+++ src/dto/RetoAssembler.java
@@ -1,5 +1,32 @@
 package dto;
 
-public class RetoAssembler {
+import java.util.Date;
 
+import clases.Reto;
+
+public class RetoAssembler {
+	private static RetoAssembler instance;
+	
+	private RetoAssembler() {}
+	
+	public static RetoAssembler getInstance() {
+		if (instance == null) {
+			instance = new RetoAssembler();
+		}
+		return instance;
+	}
+	
+	public RetoDTO retoToDTO(Reto reto) {
+		RetoDTO dto = new RetoDTO();
+		
+		dto.setNombre(reto.getNombre());
+		dto.setFechaIni(reto.getFechaIni());
+		dto.setFechaFin(reto.getFechaFin());
+		dto.setDistancia(reto.getDistancia());
+		dto.setTiempoObjetivo(reto.getTiempoObjetivo());
+		dto.setDeporte(reto.getDeporte());
+		
+		
+		return dto;
+	}
 }
